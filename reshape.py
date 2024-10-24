cb05109e7fb7c402378de0e27ac6719c8891d457
247800 <114662833+247800@users.noreply.github.com>
2024-10-24 19:40:29 +0200

    Update reshape.py

--- reshape.py
+++ reshape.py
@@ -7,10 +7,9 @@ shape = (500000,1,)
 tensor = torch.rand(shape)
 print(f"Tensor Shape:", tensor.shape)
 
-# chceme dostat tvar (batch size, 2^16, 1)
+# pokud chceme tenzor ve tvaru (batch size, 2^16, 1)
 # 500 000/(2^16)=7,6; zaokrouhlime a ziskame velikost: batch = 8;
 # 8*(2^16)=524 288; abychom zaplnili batch, musime rozsirit tenzor z 500tis na 524 288;
-
 pad = (0,0,0,24288)
 padded_tensor = torch.nn.functional.pad(tensor,pad, mode='constant', value=0)
 print(f"Padded Tensor Shape:", padded_tensor.shape)
@@ -18,6 +17,10 @@ print(f"Padded Tensor Shape:", padded_tensor.shape)
 # reshape padded tenzoru do pozadovaneho tvaru (batch, 2^16, 1)
 reshaped_tensor = padded_tensor.reshape((8,65536,1))
 print(f"Reshaped Tensor Shape:", reshaped_tensor.shape)
+
+#jiny zpusob
+size = pow(2,16)
+batch = tensor[size * n:size * (n+1)]
 
 # zkraceni puvodniho tenzoru: chceme zkratit cas nahravky, potrebujeme tedy snizit pocet vzorku
 trimmed_tensor = tensor[0:250000,:]
