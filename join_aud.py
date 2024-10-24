08432b77777d70c60c8b046bd9dddd6e9ab8b406
247800 <114662833+247800@users.noreply.github.com>
2024-10-24 21:16:04 +0200

    Add files via upload

---
+++ join_aud.py
@@ -0,0 +1,17 @@
+import torch
+import torchaudio
+import matplotlib.pyplot as plt
+from torchaudio.transforms import Spectrogram
+
+waveform, sample_rate = torchaudio.load("synth.wav")
+print("Tensor shape:", waveform.shape)
+
+joined_tensor = torch.cat((waveform, waveform), 1)
+print("Joined Tensor shape:", joined_tensor.shape)
+
+plt.figure()
+plt.plot(joined_tensor.t().numpy())
+plt.xlabel("Sample")
+plt.ylabel("Amplitude")
+plt.title("Joined Tensor Waveform")
+plt.show()
